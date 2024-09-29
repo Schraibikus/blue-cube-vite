59206fcec4742a2dfb449dc1958303b68adf49f9
Roman Ulitin <kurt-77@mail.ru>
2024-09-29 13:39:46 +0500

    нормализовал стили

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import './App.css'
+import s from "./app.module.scss";
 
 function App() {
-
   return (
     <>
-        
-      <h1>Vite + React</h1>
-      
+      <h1 className={s.title}>Vite + React</h1>
+      <p className={s.paragraph}>5000</p>
+      <p>5000</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

47f6a213daa4972d83ba58b21a6a58f83aea044f
Roman Ulitin <kurt-77@mail.ru>
2024-10-14 15:25:44 +0500

    попытка сделать свой debounce

--- src/components/Search/SearchInput.tsx
+++ src/components/Search/SearchInput.tsx
@@ -4,17 +4,40 @@ import { getSearchItems } from "../../store/modules/items";
 import { setSearchValue } from "../../store/modules/items/itemsSlice";
 import styles from "./Search.module.scss";
 import getClubCountWordForm from "../../utils/getAllClubCountWordForm";
+// import { useEffect, useState } from "react";
 
 export const SearchInput = () => {
+  // const useDebouncedValue = (value: string, delay: number) => {
+  //   const [debouncedValue, setDebouncedValue] = useState(value);
+
+  //   useEffect(() => {
+  //     const handler = setTimeout(() => {
+  //       setDebouncedValue(value);
+  //     }, delay);
+
+  //     return () => {
+  //       clearTimeout(handler);
+  //     };
+  //   }, [value, delay]);
+
+  //   return debouncedValue;
+  // };
+
   const dispatch = useAppDispatch();
   const searchValue = useAppSelector((state) => state.items.searchValue);
   const foundItems = useAppSelector((state) => state.items.foundItems);
+
+  // const debouncedSearch = useDebouncedValue(searchValue, 500);
 
   const filteredItems = foundItems.filter((item) =>
     item.title.toLowerCase().includes(searchValue.toLowerCase())
   );
 
-  const handleSearchInputChange = (event: { target: { value: string } }) => {
+  // useEffect(() => {
+  //   dispatch(getSearchItems(debouncedSearch));
+  // }, [dispatch, debouncedSearch]);
+
+  const handleInputChange = (event: { target: { value: string } }) => {
     dispatch(setSearchValue(event.target.value));
     dispatch(getSearchItems(event.target.value));
   };
@@ -44,16 +67,21 @@ export const SearchInput = () => {
               onClick={clearInput}
             />
           )}
+          {/* <input
+            type="text"
+            value={searchValue}
+            placeholder="Поиск..."
+            onChange={(event) => dispatch(setSearchValue(event.target.value))}
+          /> */}
           <DebounceInput
-            minLength={2} // минимальная длина ввода перед тем, как будет вызвана функция handleSearchInputChange
-            debounceTimeout={500} // задержка перед тем, как будет вызвана функция handleSearchInputChange
-            onChange={handleSearchInputChange}
+            minLength={2} // минимальная длина ввода перед тем, как будет вызвана функция handleInputChange
+            debounceTimeout={500} // задержка перед тем, как будет вызвана функция handleInputChange
+            onChange={handleInputChange}
             value={searchValue}
             placeholder="Поиск..."
           />
         </div>
       </div>
-      {/* <div className={styles.search__items}>{renderItems()}</div> */}
     </div>
   );
 };

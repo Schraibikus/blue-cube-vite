622502e032f9afaa4497f4960fb5a1a84dd899c7
Roman Ulitin <kurt-77@mail.ru>
2024-10-16 14:45:30 +0500

    пофиксил наименование функций в store

--- src/store/modules/items/thunk.ts
+++ src/store/modules/items/thunk.ts
@@ -1,13 +1,13 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { AxiosError } from "axios";
-import { fetchGetItems, fetchGetOneItem, fetchSearchItems } from "./apis";
+import { fetchItems, fetchOneItem, fetchSearchItems } from "./apis";
 import { toast } from "react-toastify";
 
 export const getItems = createAsyncThunk(
   "items/getItems",
   async (page: number) => {
     try {
-      const { data } = await fetchGetItems(page);
+      const { data } = await fetchItems(page);
       return data;
     } catch (error) {
       if (error instanceof AxiosError) return toast.error(error.message);
@@ -19,7 +19,7 @@ export const getItem = createAsyncThunk(
   "items/getItem",
   async (productId: string) => {
     try {
-      const data = await fetchGetOneItem(productId);
+      const data = await fetchOneItem(productId);
       return data;
     } catch (error) {
       if (error instanceof AxiosError) return toast.error(error.message);
